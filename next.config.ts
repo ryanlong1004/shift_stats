07d234289190b9760080317eacd92e99aac65253
Ryan Long <ryan@rlong.dev>
2026-03-19 19:01:34 -0400

    Use native output on Vercel deployments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-};
+const nextConfig: NextConfig =
+  process.env.VERCEL === "1"
+    ? {}
+    : {
+        output: "standalone",
+      };
 
 export default nextConfig;
